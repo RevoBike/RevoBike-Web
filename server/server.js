994650d34580e0bb04efc0fc7e01581e3c788060
Tesfamariam21 <tctesfa21@gmail.com>
2025-02-22 13:37:41 +0300

    Modify server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require("./src/config/db");
+
+dotenv.config(); //load env
 const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server running on port ${PORT}');
 });
+
+
+
+
