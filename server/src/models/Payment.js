31f21e1b5d33570a5872ed522ae8e41782a60f90
Tesfamariam21 <tctesfa21@gmail.com>
2025-02-22 13:37:46 +0300

    add Payment schema

---
+++ server/src/models/Payment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const PaymentSchema = new mongoose.Schema({
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User", 
+        required: true 
+    },
+    ride: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Ride" 
+    },
+    amount: { 
+        type: Number, 
+        required: true 
+    },
+    status: { 
+        type: String, 
+        enum: ["pending", "completed", "failed"], 
+        default: "pending" 
+    },
+}, { timestamps: true });
+
+const Payment = mongoose.model("Payment", PaymentSchema);
+
+module.exports = Payment;
