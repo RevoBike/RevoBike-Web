151f538378991c91df8478ff839aa9e25e408945
Bz14 <bezueyerusalem@gmail.com>
2025-04-21 17:31:28 +0300

    feat: Add maintenance routes and controller for bike maintenance management
    - Implemented maintenanceController with functions to manage bikes under maintenance.
    - Added maintenanceRoute for handling API requests related to bike maintenance.
    - Updated server.js to include maintenance routes.
    - Enhanced bikeController to manage bike status and metrics.
    - Updated userController to include admin email notifications for new admins.
    - Added validation and error handling for bike and station operations.
    - Introduced user metrics retrieval in userController.
    - Updated stationController to prevent deletion of stations with available bikes.
    - Added functionality to update bike details and manage bike maintenance history.

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,25 +1,34 @@
-const express = require('express');
-const { 
-    getAllUsers, 
-    getUserById, 
-    updateUser, 
-    deleteUser,
-    createAdmin, 
-    verifyUser
-} = require('../controllers/userController');
-const { protect, authorizeRoles } = require('../middlewares/middleware');
+const express = require("express");
+const {
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  createAdmin,
+  verifyUser,
+  getUserMetrics,
+  getAllUsersInTheSystem,
+} = require("../controller/userController");
+const { protect, authorizeRoles } = require("../middlewares/middleware");
 
 const router = express.Router();
 
-router.get('/', protect, authorizeRoles('admin'), getAllUsers);
-router.get('/:id', protect, getUserById);
-router.put('/:id', protect, updateUser);
-router.delete('/:id', protect, authorizeRoles('admin'), deleteUser);
+router.get("/all", protect, getAllUsersInTheSystem);
+router.get("/", protect, authorizeRoles("admin"), getAllUsers);
+router.get("/user-metrics", protect, authorizeRoles("admin"), getUserMetrics);
+router.get("/:id", protect, getUserById);
+router.put("/:id", protect, updateUser);
+router.delete("/:id", protect, authorizeRoles("admin"), deleteUser);
 
 // to verify user (Only Admins & SuperAdmins can do this)
-router.put("/verify/:userId", protect, authorizeRoles("Admin", "SuperAdmin"), verifyUser);
+router.put(
+  "/verify/:userId",
+  protect,
+  authorizeRoles("Admin", "SuperAdmin"),
+  verifyUser
+);
 
 //for adding admin
-router.post("/admin", protect, authorizeRoles("SuperAdmin"), createAdmin);
+router.post("/", protect, authorizeRoles("SuperAdmin"), createAdmin);
 
 module.exports = router;
